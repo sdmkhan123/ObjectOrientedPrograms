2542297ecf245c8e24b9391f01c8648163058752
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-17 04:11:11 +0530

    Create a JSON file having Inventory Details

--- InventoryDetails/Inventory.cs
+++ InventoryDetails/Inventory.cs
@@ -9,13 +9,5 @@ namespace ObjectOrientedPrograms
         public string Name { get; set; }
         public double Weight { get; set; }
         public double Price { get; set; }
-
-        //Parameterized Constructor
-/*        public Inventory(string Name, double Weight, double Price)
-        {
-            this.Name = Name;
-            this.Weight = Weight;
-            this.Price = Price;
-        }*/
     }
 }
